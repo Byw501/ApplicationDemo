f0bedee944691ea52672d2df633133a911c7b294
Byw501 <2522112571@qq.com>
2024-08-08 17:15:30 +0800

    2

--- Assets/Script/MovementControl.cs
+++ Assets/Script/MovementControl.cs
@@ -6,6 +6,7 @@ public class MovementControl : MonoBehaviour
 {
     public float cubeSpeed;
     public GameObject blocker;
+    public Vector3 sizeMax;
     private bool isMove = false;
     private Vector3 direction = Vector3.zero;
     private GameObject checkpoint;
@@ -13,6 +14,7 @@ public class MovementControl : MonoBehaviour
     void Start()
     {
         checkpoint = GameObject.Find("Checkpoint");
+        sizeMax -= Vector3.one;
     }
 
     // Update is called once per frame
@@ -54,7 +56,37 @@ public class MovementControl : MonoBehaviour
 
         if (isMove)
         {
-            transform.Translate(direction * cubeSpeed * Time.deltaTime);
+            transform.position += direction * cubeSpeed * Time.deltaTime;
+            if(transform.position.x > sizeMax.x)
+            {
+                transform.position = new Vector3(sizeMax.x,transform.position.y, transform.position.z);
+                StopMotion();
+            }
+            if (transform.position.x < 0f)
+            {
+                transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+                StopMotion();
+            }
+            if (transform.position.y > sizeMax.y)
+            {
+                transform.position = new Vector3(transform.position.x, sizeMax.y, transform.position.z);
+                StopMotion();
+            }
+            if (transform.position.y < 0f)
+            {
+                transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+                StopMotion();
+            }
+            if (transform.position.z > sizeMax.z)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, sizeMax.z);
+                StopMotion();
+            }
+            if (transform.position.z < 0f)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+                StopMotion();
+            }
         }
     }
 
@@ -72,10 +104,26 @@ public class MovementControl : MonoBehaviour
         }
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Unbreakable")
+        {
+            StopMotion();
+        }
+        else if (collision.gameObject.tag == "Breakable")
+        {
+            StopMotion();
+            Destroy(collision.gameObject);
+            SpawnBlock();
+        }
+    }
+
     private void StopMotion()
     {
         isMove = false;
-        transform.Translate(-direction * 0.1f);
+        Vector3 pos = transform.position;
+        pos = new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), Mathf.Round(pos.z));
+        transform.position = pos;
     }
 
     private void SpawnBlock()
